87c9647b19ce9b3b800c6c454d1c37054483e95e
Ari Guzzi <argu4451@colorado.edu>
2023-10-26 12:40:25 -0600

    chapter 1-6

--- LearningJavaScript:TypeScript/modern-typescript/sandbox.ts
+++ LearningJavaScript:TypeScript/modern-typescript/sandbox.ts
@@ -1,9 +1,48 @@
-const character = 'mario';
+/* Chapter 2 */
+// const character = 'mario';
 
-console.log(character);
+// console.log(character);
 
-const inputs = document.querySelectorAll('input');
+// const inputs = document.querySelectorAll('input');
 
-inputs.forEach(input => {
-  console.log(input);
-});
+// inputs.forEach(input => {
+//   console.log(input);
+// });
+/* Chapter 3 */
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+// character = 20;
+character = 'luigi';
+
+// age = 'yoshi';
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+// console.log(area('hello'));
+console.log(circ(7.5));
+
+/* Chapter 4 */
+/* Chapter 5 */
+/* Chapter 6 */
+/* Chapter 7 */
+/* Chapter 8 */
+/* Chapter 9 */
+/* Chapter 10 */
+/* Chapter 11 */
+/* Chapter 12 */
+/* Chapter 13 */
+/* Chapter 14 */
+/* Chapter 15 */
+/* Chapter 16 */
+/* Chapter 17 */
+/* Chapter 18 */
+/* Chapter 19 */
+/* Chapter 20 */
